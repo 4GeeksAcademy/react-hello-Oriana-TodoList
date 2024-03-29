617fdc9bfdcce7fbac0a0f677a02c21b7802e73f
Orianac0698 <150297944+Orianac0698@users.noreply.github.com>
2024-03-29 10:38:41 +0000

    message

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,68 @@
-import React from "react";
+import React,{useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+	const [newInput,setNewInput]= useState("");
+	const [addTodo , setAddTodo]=useState(["A test task"]);
+
+	
+
+	const handleChange= (e)=>{
+		setNewInput(e.target.value);
+		
+	}
+	const handleKeyDown = (e) => {
+		if (e.key === "Enter") {
+	
+			setAddTodo(addTodo.concat(newInput));
+			setNewInput("");
+		}
+	  };
+
+	return(
+
+	
+		<div className="container ">
+
+			<div className="mt-2 text-center ">
+				<h1>My todo list</h1>	
+			</div>
+		
+			<div className="m-2">
+				
+				<input type="text" class="form-control" placeholder="Add new task" onChange={handleChange} 
+				onKeyDown={handleKeyDown}/>
+					
+
+				<ul class="list-group list-group-flush">
+			
+					{addTodo.map ((item, index) => (
+						
+
+						<li class=" list-group-item d-flex justify-content-between alaing-items-centeralign-items-center mt-4 ">
+							{item}
+							<button className="btn-close" onClick={() => setAddTodo(
+								addTodo.filter(
+									(t,curretIndex) => index != curretIndex
+								))}></button>
+
+							
+						
+						</li>				
+					))}
+				     
+				</ul>
+			</div>
+
+			<div className="d-flex flex-row m-2">{addTodo.length} Pending tasks</div>
+
 		</div>
-	);
+
+	)
 };
 
 export default Home;
